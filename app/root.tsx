e622909084d7dac1c0744fe95fbd93389868bcc4
Chance Strickland <hi@chance.dev>
2024-01-30 14:17:48 -0800

    homepage

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { useForm } from "@conform-to/react";
 import { parse } from "@conform-to/zod";
 import { cssBundleHref } from "@remix-run/css-bundle";
@@ -10,7 +11,6 @@ import {
 	type MetaFunction,
 } from "@remix-run/node";
 import {
-	Form,
 	Link,
 	Links,
 	LiveReload,
@@ -22,25 +22,14 @@ import {
 	useFetchers,
 	useLoaderData,
 	useMatches,
-	useSubmit,
 } from "@remix-run/react";
 import { withSentry } from "@sentry/remix";
-import { useRef } from "react";
 import { HoneypotProvider } from "remix-utils/honeypot/react";
 import { z } from "zod";
 import { GeneralErrorBoundary } from "./components/error-boundary.tsx";
 import { ErrorList } from "./components/forms.tsx";
 import { EpicProgress } from "./components/progress-bar.tsx";
-import { SearchBar } from "./components/search-bar.tsx";
 import { useToast } from "./components/toaster.tsx";
-import { Button } from "./components/ui/button.tsx";
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuPortal,
-	DropdownMenuTrigger,
-} from "./components/ui/dropdown-menu.tsx";
 import { Icon, href as iconsHref } from "./components/ui/icon.tsx";
 import { EpicToaster } from "./components/ui/sonner.tsx";
 import tailwindStyleSheetUrl from "./styles/tailwind.css";
@@ -49,13 +38,12 @@ import { ClientHintCheck, getHints, useHints } from "./utils/client-hints.tsx";
 import { prisma } from "./utils/db.server.ts";
 import { getEnv } from "./utils/env.server.ts";
 import { honeypot } from "./utils/honeypot.server.ts";
-import { combineHeaders, getDomainUrl, getUserImgSrc } from "./utils/misc.tsx";
+import { combineHeaders, getDomainUrl } from "./utils/misc.tsx";
 import { useNonce } from "./utils/nonce-provider.ts";
 import { useRequestInfo } from "./utils/request-info.ts";
 import { type Theme, setTheme, getTheme } from "./utils/theme.server.ts";
 import { makeTimings, time } from "./utils/timing.server.ts";
 import { getToast } from "./utils/toast.server.ts";
-import { useOptionalUser, useUser } from "./utils/user.ts";
 
 export const links: LinksFunction = () => {
 	return [
@@ -223,39 +211,48 @@ function Document({
 function App() {
 	const data = useLoaderData<typeof loader>();
 	const nonce = useNonce();
-	const user = useOptionalUser();
 	const theme = useTheme();
 	const matches = useMatches();
-	const isOnSearchPage = matches.find((m) => m.id === "routes/users+/index");
-	const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />;
 	useToast(data.toast);
+
+	React.useEffect(() => {
+		console.log(matches);
+	}, [matches]);
 
 	return (
 		<Document nonce={nonce} theme={theme} env={data.ENV}>
 			<div className="flex h-screen flex-col justify-between">
 				<header className="container py-6">
-					<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-						<Logo />
-						<div className="ml-auto hidden max-w-sm flex-1 sm:block">
-							{searchBar}
-						</div>
-						<div className="flex items-center gap-10">
-							{user ? (
-								<UserDropdown />
-							) : (
-								<Button asChild variant="default" size="lg">
-									<Link to="/login">Log In</Link>
-								</Button>
-							)}
-						</div>
-						<div className="block w-full sm:hidden">{searchBar}</div>
-					</nav>
+					<div className="flex justify-between gap-6">
+						<h1>
+							<Link to="/">Chance and Morgan</Link>
+						</h1>
+						<nav className="flex items-center justify-center" aria-label="main">
+							<ul className="flex flex-wrap items-center justify-end gap-x-4 gap-y-1">
+								{[
+									{ to: "/", label: "Event" },
+									{ to: "/location", label: "Location" },
+									{ to: "/travel", label: "Travel" },
+									{ to: "/photos", label: "Photos" },
+									{ to: "/gifts", label: "Gifts" },
+								].map((link) => {
+									return (
+										<li key={link.label}>
+											<Link to={link.to}>{link.label}</Link>
+										</li>
+									);
+								})}
+								<li>
+									<Link to="/rsvp">RSVP</Link>
+								</li>
+							</ul>
+						</nav>
+					</div>
 				</header>
 
 				<div className="flex-1">
 					<Outlet />
 				</div>
-
 				<div className="container flex justify-between pb-5">
 					<Logo />
 					<ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
@@ -290,67 +287,6 @@ function AppWithProviders() {
 }
 
 export default withSentry(AppWithProviders);
-
-function UserDropdown() {
-	const user = useUser();
-	const submit = useSubmit();
-	const formRef = useRef<HTMLFormElement>(null);
-	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button asChild variant="secondary">
-					<Link
-						to={`/users/${user.username}`}
-						// this is for progressive enhancement
-						onClick={(e) => e.preventDefault()}
-						className="flex items-center gap-2"
-					>
-						<img
-							className="h-8 w-8 rounded-full object-cover"
-							alt={user.name ?? user.username}
-							src={getUserImgSrc(user.image?.id)}
-						/>
-						<span className="text-body-sm font-bold">
-							{user.name ?? user.username}
-						</span>
-					</Link>
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuPortal>
-				<DropdownMenuContent sideOffset={8} align="start">
-					<DropdownMenuItem asChild>
-						<Link prefetch="intent" to={`/users/${user.username}`}>
-							<Icon className="text-body-md" name="avatar">
-								Profile
-							</Icon>
-						</Link>
-					</DropdownMenuItem>
-					<DropdownMenuItem asChild>
-						<Link prefetch="intent" to={`/users/${user.username}/notes`}>
-							<Icon className="text-body-md" name="pencil-2">
-								Notes
-							</Icon>
-						</Link>
-					</DropdownMenuItem>
-					<DropdownMenuItem
-						asChild
-						// this prevents the menu from closing before the form submission is completed
-						onSelect={(event) => {
-							event.preventDefault();
-							submit(formRef.current);
-						}}
-					>
-						<Form action="/logout" method="POST" ref={formRef}>
-							<Icon className="text-body-md" name="exit">
-								<button type="submit">Logout</button>
-							</Icon>
-						</Form>
-					</DropdownMenuItem>
-				</DropdownMenuContent>
-			</DropdownMenuPortal>
-		</DropdownMenu>
-	);
-}
 
 /**
  * @returns the user's theme preference, or the client hint theme if the user
